1ee4712ca67b01c264a86024be0f16524ec892aa
Manuel <manuel@prototype.berlin>
2019-04-23 11:37:37 +0200

    Fix path for Cordova 8

--- scripts/androidBeforeCompile.js
+++ scripts/androidBeforeCompile.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const androidManifest = fs.readFileSync(process.cwd() + '/platforms/android/AndroidManifest.xml', 'utf-8');
+const androidManifest = fs.readFileSync(process.cwd() + '/platforms/android/app/src/main/AndroidManifest.xml', 'utf-8');
 const matches = androidManifest.match(/^.*(com.facebook.sdk.ApplicationId).*/gm);
 
 if (matches.length > 1) {
-  fs.writeFileSync(process.cwd() + '/platforms/android/AndroidManifest.xml', androidManifest.replace(/^.*(com.facebook.sdk.ApplicationId).*/m,''), 'utf-8');
+  fs.writeFileSync(process.cwd() + '/platforms/android/app/src/main/AndroidManifest.xml', androidManifest.replace(/^.*(com.facebook.sdk.ApplicationId).*/m,''), 'utf-8');
 }
